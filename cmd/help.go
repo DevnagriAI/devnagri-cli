fea0ecbe1f31441194d73beb3476125992ddcac9
Abhinav Sharma <abhi18av@gmail.com>
2017-12-15 14:42:59 +0530

    working towards having a first sample release

--- cmd/help.go
+++ cmd/help.go
@@ -23,13 +23,8 @@ import (
 // helpCmd represents the help command
 var helpCmd = &cobra.Command{
 	Use:   "help",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Prints out a short manual for devnagri",
+	Long:  `A long explanation about help`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("help called")
 	},
--- cmd/validate.go
+++ cmd/validate.go
@@ -23,13 +23,8 @@ import (
 // validateCmd represents the validate command
 var validateCmd = &cobra.Command{
 	Use:   "validate",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A brief description of validate command",
+	Long:  `A long description of validate command.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("validate called")
 	},
