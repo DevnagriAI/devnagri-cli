dfe079f484459fbd9b92e66f21c3391b9e702b99
Abhinav Sharma <abhi18av@gmail.com>
2017-12-18 12:27:01 +0530

    routine commit

--- utils/devnagri_config.go
+++ utils/devnagri_config.go
@@ -20,10 +20,13 @@ func main() {
 	// Confirm which config file is used
 	fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
 
-	x := viper.Get("auth.username") // returns string
+	x := viper.Get("client_id") // returns string
 	//port := viper.GetInt("prod.port") // returns integer
 	fmt.Printf("\n\nValue: %v, Type: %T\n", x, x)
 
+
+
+	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("prod.none") {
 		fmt.Printf("\n")
 		log.Fatal("missing the \"none\" field")
