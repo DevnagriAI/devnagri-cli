ad047d59b5dcbecf41d3ecd63acfc39c7f209de2
Abhinav Sharma <abhi18av@gmail.com>
2017-12-18 14:22:31 +0530

    working on the config validation

--- utils/devnagri_config.go
+++ utils/devnagri_config.go
@@ -20,18 +20,13 @@ func main() {
 	// Confirm which config file is used
 	fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
 
-	x := viper.Get("client_id") // returns string
+	x := viper.GetString("ClientID") // returns string
 	//port := viper.GetInt("prod.port") // returns integer
 	fmt.Printf("\n\nValue: %v, Type: %T\n", x, x)
 
-
-
 }
 
-
-func validateAllFields(){
-
-
+func validateAllFields() {
 
 	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("ClientID") {
@@ -39,54 +34,34 @@ func validateAllFields(){
 		log.Fatal("missing the \"none\" field")
 	}
 
-
-
-
-
 	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("ClientSecret") {
 		fmt.Printf("\n")
 		log.Fatal("missing the \"none\" field")
 	}
 
-
-ProjectKey
-
-
 	// To validate the presence of a certain field in the YAML file
-	if !viper.IsSet("prod.none") {
+	if !viper.IsSet("ProjectKey") {
 		fmt.Printf("\n")
 		log.Fatal("missing the \"none\" field")
 	}
 
-
-
-LocalizationFolder
-
 	// To validate the presence of a certain field in the YAML file
-	if !viper.IsSet("prod.none") {
+	if !viper.IsSet("LocalizationFolder") {
 		fmt.Printf("\n")
 		log.Fatal("missing the \"none\" field")
 	}
 
-
-
-GlobalPreferenceInCaseOfMergeConflict
 	// To validate the presence of a certain field in the YAML file
-	if !viper.IsSet("prod.none") {
+	if !viper.IsSet("GlobalPreferenceInCaseOfMergeConflict") {
 		fmt.Printf("\n")
 		log.Fatal("missing the \"none\" field")
 	}
 
-
-LanguagesToBeTranslated:
-
-
 	// To validate the presence of a certain field in the YAML file
-	if !viper.IsSet("prod.none") {
+	if !viper.IsSet("LanguagesToBeTranslated") {
 		fmt.Printf("\n")
 		log.Fatal("missing the \"none\" field")
 	}
-
 
 }
