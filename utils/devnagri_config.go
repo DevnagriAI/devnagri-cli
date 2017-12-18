5b036bbc11573d314478ef043e1e00ee6b5b2b53
Abhinav Sharma <abhi18av@gmail.com>
2017-12-18 11:58:45 +0530

    tweaking the config settings and validation

--- utils/devnagri_config.go
+++ utils/devnagri_config.go
@@ -13,11 +13,12 @@ import (
 func main() {
 
 	viper.SetConfigFile("./env.json")
-	port := viper.Get("prod.port") // returns string
+	port := viper.Get("dev.port") // returns string
 	//port := viper.GetInt("prod.port") // returns integer
-	fmt.Printf("Value: %v, Type: %T\n", port, port)
+	fmt.Printf("\n\nValue: %v, Type: %T\n", port, port)
 
 	if !viper.IsSet("prod.none") {
+		fmt.Printf("\n")
 		log.Fatal("missing the \"none\" field")
 	}
 
