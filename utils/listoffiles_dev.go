870df9b5cecb41f956b0a48dfd72624090ea78c1
Abhinav Sharma <abhi18av@gmail.com>
2017-12-19 11:57:35 +0530

    working on adding the support for RootDir

--- utils/listoffiles_dev.go
+++ utils/listoffiles_dev.go
@@ -9,9 +9,29 @@ import (
 )
 
 func main()  {
-fmt.Println("Hello, files")	
-
-ioutil.ReadDir
 
 
+
+	viper.SetConfigFile("./.devnagri.yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	// Confirm which config file is used
+	fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
+
+	x := viper.GetString("RootDir") // returns string
+	//port := viper.GetInt("prod.port") // returns integer
+	fmt.Printf("\n\nValue: %v, Type: %T\n", x, x)
+
+
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
