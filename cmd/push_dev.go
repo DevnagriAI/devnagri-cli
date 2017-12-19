bf23a25a9853bc916f0e59c6e7fcfb7d6ea2abb7
Abhinav Sharma <abhi18av@gmail.com>
2017-12-19 12:29:02 +0530

    added the source code for leaktest

---
+++ cmd/push_dev.go
@@ -0,0 +1,22 @@
+package main
+
+
+import (
+	
+	"fmt"
+	"io/ioutil"
+	"log"
+	"github.com/spf13/viper"
+	"leaktest"
+	"devnagri-cli/utils"
+)
+
+func main()  {
+
+
+	files := listoffiles_dev.main()
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
