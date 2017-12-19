150979daf945de49e943a111288e411d7c99d617
Abhinav Sharma <abhi18av@gmail.com>
2017-12-19 19:42:39 +0530

    added the requisite commands

--- cmd/push_dev.go
+++ cmd/push_dev.go
@@ -1,34 +1,28 @@
 package main
 
-
 import (
-	
 	"fmt"
-	"log"
+
 	"gopkg.in/resty.v1"
 )
 
-HEADERS
-Accept application/json
-Content-Type multipart/form-data
-BODY
-client_id3
-client_secretHfU2hKMpArJaZVUljxNBoBYgWlWDK0D7Rmh6u3wn
-project_keyd41d8cd98f00b204e9800998ecf8427e
-file[0][hash]31c86620ffe30c99b6e7cb7c2b9f4c6284945e1d4d4f2e9b7445ac52c85bf38d
-file[0][name]asdasd
-file[0][extension]xml
-file[0][file_type]xml
-file[1][hash]31c86620ffe30c99b6e7cb7c2b9f4c6284945e1d4d4f2e9b7445ac52c85bf38d
-file[1][name]asdasd
-file[1][extension]xml
-file[1][file_type]xml
-file[0][file][object Object]
-file[1][file][object Object]
-func main() {
+/*
+file[0][hash]
+file[0][name]
+file[0][extension]
+file[0][file_type]
+file[1][hash]
+file[1][name]
+file[1][extension]
+file[1][file_type]
+file[0][file]
+file[1][file]
+*/
 
+func main() {
 	resp, err := resty.R().
 		SetHeader("Accept", "application/json").
+		SetHeader("Content-Type", "multipart/form-data").
 		SetAuthToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ2ODhlOGIwNTNjZmEwZDlmNWZjZjA5NDBjYWI1MTRiNzM2NDgyZDYxNWM0ZWZiY2RkYzUzOTc5Yzk2YjUxZGE4MmJjZDQ0MGI5YjdmMmMwIn0.eyJhdWQiOiIxIiwianRpIjoiNDY4OGU4YjA1M2NmYTBkOWY1ZmNmMDk0MGNhYjUxNGI3MzY0ODJkNjE1YzRlZmJjZGRjNTM5NzljOTZiNTFkYTgyYmNkNDQwYjliN2YyYzAiLCJpYXQiOjE1MTM2NzgyMjUsIm5iZiI6MTUxMzY3ODIyNSwiZXhwIjoxNTQ1MjE0MjI1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.IAuBFik92MfyTmJExDi7AuWPWNxFiCEjHU080PpYWNVafORYaydVJbizeY31ZNzquUFMND8shjvJxK_mmRKE3lcFqcnWnJDvSsSgFdavJ9z_v1tyq0e6hMPtqncRpRRkxmYhZsVVYjP4uLdUzdNhnJC9vDJtU2icv2OzR2zDgzvMhphfWlDtTaWNywJB2EHmun-hkbk4TLjx7rjAGy-yhskh_-3rEkvll_IhG4dSv87u6l-QCiZwS5OEBqCRcOWWqZtqghIDyktSZc1WNJVsvxbIdCkB8v44qGRMzkWm3LxRbqRl6WwcC5OZcqy-Uw9PLqlEEDMsVlO0GG9sEVPHZ5Oheh8ftNf_AiqQxamTzIwli_0Yjjwgj6U1tp37rDDYcgRCyYbwoBubWr7Maw45fLZvrrH8khndSRfRpY3o7S42UoCa3AVEWmHpuiRaqYAsSbqGsU_HD1urIsZTa19t8x6ocw8vmcQS9cJgp116QxMWYz4qdUqjpPdoxRDRehkh4ooY6q9e0DyRBbq1aXHGyqkhXWlqYUotd08GXS-nMGUwMOfrgalYvCycHSZdMAEgsfeZmKSaDRNNrRj39_JimsU6VcTSwdC99wRIPU6Yz70aIDJO0xhQA0S6j6KbwUUebiKtHoT3fDewEnLVctF7HCntnELqTIVDIlcwzu5ESxs").
 
 		//SetHeader("Content-Type", "multipart/form-data").
@@ -36,12 +30,11 @@ func main() {
 			"client_id":     "4",
 			"client_secret": " uTBn7fxknAPpmw9AiEXyIro7X8mP0JhkqPtvBS28",
 			"project_key":   "d41d8cd98f00b204e9800998ecf8427e"}).
-		Post("http://192.168.60.10/api/project/status")
-		//Post("https://requestb.in/16pfgiv1")
+		//Post("http://192.168.60.10/api/project/status")
+		Post("https://requestb.in/1j9cr3f1")
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(resp)
 }
-
