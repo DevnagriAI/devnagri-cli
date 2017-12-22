e27d02f3eb3e6225d4f65382e6acac7e2d2a42f4
Abhinav Sharma <abhi18av@gmail.com>
2017-12-22 15:53:32 +0530

    added the _dev folder

---
+++ _dev/filehash_dev.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("books.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%x", h.Sum(nil))
+}
