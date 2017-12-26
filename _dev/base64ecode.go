3bd3210520afb88e2e9ebf69b1f9e93cf020c6e7
Abhinav Sharma <abhi18av@gmail.com>
2017-12-26 19:10:28 +0530

    working on the decoding the cypher

--- _dev/base64ecode.go
+++ _dev/base64ecode.go
@@ -3,32 +3,19 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"log"
 )
 
-func base64Encode(src []byte) []byte {
-	return []byte(base64.StdEncoding.EncodeToString(src))
-}
-
-func base64Decode(src []byte) ([]byte, error) {
-	return base64.StdEncoding.DecodeString(string(src))
-}
-
 func main() {
-	// encode
+	str := base64.StdEncoding.EncodeToString([]byte("Hello, playground"))
+	fmt.Println(str)
 
-	hello := "PHJlc291cmNlcyB0b29sczppZ25vcmU9IkV4dHJhVHJhbnNsYXRpb24iIHhtbG5zOnRvb2xzPSJodHRwOi8vc2NoZW1hcy5hbmRyb2lkLmNvbS90b29scyI+CiAgICA8c3RyaW5nIG5hbWU9ImFwcF9uYW1lIj48L3N0cmluZz4KICAgIDxzdHJpbmcgbmFtZT0iaGludF9hY3R1YWwiPjwvc3RyaW5nPgogPC9yZXNvdXJjZXM+"
+	cypher := " PHJlc291cmNlcyB0b29sczppZ25vcmU9IkV4dHJhVHJhbnNsYXRpb24iIHhtbG5zOnRvb2xzPSJodHRwOi8vc2NoZW1hcy5hbmRyb2lkLmNvbS90b29scyI+CiAgICA8c3RyaW5nIG5hbWU9ImFwcF9uYW1lIj48L3N0cmluZz4KICAgIDxzdHJpbmcgbmFtZT0iaGludF9hY3R1YWwiPjwvc3RyaW5nPgogPC9yZXNvdXJjZXM+"
 
-	debyte := base64Encode([]byte(hello))
-	fmt.Println(debyte)
-	// decode
-	enbyte, err := base64Decode(debyte)
+	data, err := base64.StdEncoding.DecodeString(cypher)
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatal("error:", err)
 	}
 
-	if hello != string(enbyte) {
-		fmt.Println("hello is not equal to enbyte")
-	}
-
-	fmt.Println(string(enbyte))
+	fmt.Printf("%q\n", data)
 }
