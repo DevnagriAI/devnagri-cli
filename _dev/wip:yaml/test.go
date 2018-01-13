95bd3d589e0cc84bf5fd31691a1d91fa27e55bfc
Abhinav Sharma <abhi18av@gmail.com>
2018-01-13 16:21:49 +0530

    moving the folder to github/org/package location

--- _dev/wip:yaml/test.go
+++ _dev/wip:yaml/test.go
@@ -1,26 +1,12 @@
 package main
 
 import (
+	config "devnagri-cli/config"
 	"fmt"
-	"github.com/spf13/viper"
-	"log"
 )
 
 func main() {
 
-	ViperConfig()
-	fmt.Println(ClientID)
-
-	viper.SetConfigFile("./.devnagri.yaml")
-
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file, %s", err)
-	}
-
-	// Confirm which config file is used
-	fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
-
-	ClientID := viper.GetString("ClientID") // returns string
-	fmt.Printf("\n\nValue: %v, Type: %T\n", ClientID, ClientID)
+	fmt.Println(config.ClientID)
 
 }
