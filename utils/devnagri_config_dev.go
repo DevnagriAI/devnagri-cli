032032314574e1d817477a53dc6e36edb6665b89
Abhinav Sharma <abhi18av@gmail.com>
2018-01-13 14:37:32 +0530

    working on the yaml file validations

--- utils/devnagri_config_dev.go
+++ utils/devnagri_config_dev.go
@@ -20,11 +20,28 @@ func main() {
 	// Confirm which config file is used
 	fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
 
-	x := viper.GetString("ClientID") // returns string
-	//port := viper.GetInt("prod.port") // returns integer
-	fmt.Printf("\n\nValue: %v, Type: %T\n", x, x)
-
 	validateAllFields()
+
+	ClientID := viper.GetString("ClientID") // returns string
+	fmt.Printf("\n\nValue: %v, Type: %T\n", ClientID, ClientID)
+
+	ClientSecret := viper.GetString("ClientSecret") // returns string
+	fmt.Printf("\n\nValue: %v, Type: %T\n", ClientSecret, ClientSecret)
+
+	ProjectKey := viper.GetString("ProjectKey") // returns string
+	fmt.Printf("\n\nValue: %v, Type: %T\n", ProjectKey, ProjectKey)
+
+	RootDir := viper.GetString("RootDir") // returns string
+	fmt.Printf("\n\nValue: %v, Type: %T\n", RootDir, RootDir)
+
+	SourceLanguage := viper.GetString("SourceLanguage") // returns string
+	fmt.Printf("\n\nValue: %v, Type: %T\n", SourceLanguage, SourceLanguage)
+
+	LanguagesToBeTranslated := viper.GetString("LanguagesToBeTranslated") // returns string
+	fmt.Printf("\n\nValue: %v, Type: %T\n", LanguagesToBeTranslated, LanguagesToBeTranslated)
+
+	GlobalPreferenceInCaseOfMergeConflict := viper.GetString("GlobalPreferenceInCaseOfMergeConflict") // returns string
+	fmt.Printf("\n\nValue: %v, Type: %T\n", GlobalPreferenceInCaseOfMergeConflict, GlobalPreferenceInCaseOfMergeConflict)
 
 }
 
@@ -33,37 +50,49 @@ func validateAllFields() {
 	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("ClientID") {
 		fmt.Printf("\n")
-		log.Fatal("missing the \"none\" field")
+		log.Fatal("missing the \"ClientID\" field")
 	}
 
 	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("ClientSecret") {
 		fmt.Printf("\n")
-		log.Fatal("missing the \"none\" field")
+		log.Fatal("missing the \"ClientSecret\" field")
 	}
 
 	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("ProjectKey") {
 		fmt.Printf("\n")
-		log.Fatal("missing the \"none\" field")
+		log.Fatal("missing the \"ProjectKey\" field")
+	}
+
+	// To validate the presence of a certain field in the YAML file
+	if !viper.IsSet("RootDir") {
+		fmt.Printf("\n")
+		log.Fatal("missing the \"RootDir\" field")
 	}
 
 	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("LocalizationFolder") {
 		fmt.Printf("\n")
-		log.Fatal("missing the \"none\" field")
+		log.Fatal("missing the \"LocalizationFolder\" field")
+	}
+
+	// To validate the presence of a certain field in the YAML file
+	if !viper.IsSet("SourceLanguage") {
+		fmt.Printf("\n")
+		log.Fatal("missing the \"SourceLanguage\" field")
 	}
 
 	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("GlobalPreferenceInCaseOfMergeConflict") {
 		fmt.Printf("\n")
-		log.Fatal("missing the \"none\" field")
+		log.Fatal("missing the \"GlobalPreferenceInCaseOfMergeConflict\" field")
 	}
 
 	// To validate the presence of a certain field in the YAML file
 	if !viper.IsSet("LanguagesToBeTranslated") {
 		fmt.Printf("\n")
-		log.Fatal("missing the \"none\" field")
+		log.Fatal("missing the \"LanguagesToBeTranslated\" field")
 	}
 
 }
