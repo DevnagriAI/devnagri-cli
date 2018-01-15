9fde2c5e9204efd999846cf37379e5efbeb1a37f
Abhinav Sharma <abhi18av@gmail.com>
2018-01-15 14:26:03 +0530

    yup, now we can generate the list of files as a json

--- _dev/listoffiles_dev.go
+++ _dev/listoffiles_dev.go
@@ -6,9 +6,40 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-
-	"github.com/Jeffail/gabs"
 )
+
+func main() {
+	var allFiles []string
+
+	root := "./en"
+	extension := "pdf"
+
+	err := filepath.Walk(root, visit(&allFiles))
+	if err != nil {
+		panic(err)
+	}
+
+	reqFiles := requiredExtensionFiles(allFiles, extension)
+	fmt.Println(reqFiles)
+	//TODO return the list of files as a json structure
+	//fmt.Println(jsonObj.String())
+}
+
+func requiredExtensionFiles(files []string, extension string) []string {
+
+	extensionRegexp := ".*." + extension + "$"
+
+	var reqFiles []string
+	for _, file := range files {
+
+		matches, _ := regexp.MatchString(extensionRegexp, file)
+		if matches == true {
+			reqFiles = append(reqFiles, file)
+
+		}
+	}
+	return reqFiles
+}
 
 func visit(files *[]string) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
@@ -18,45 +49,4 @@ func visit(files *[]string) filepath.WalkFunc {
 		*files = append(*files, path)
 		return nil
 	}
-}
-
-func main() {
-	var allFiles []string
-
-	root := "./en"
-
-	err := filepath.Walk(root, visit(&allFiles))
-	if err != nil {
-		panic(err)
-	}
-
-	jsonObj := gabs.New()
-	// Print the list of file to the console
-	for _, file := range allFiles {
-		matches, _ := regexp.MatchString(".*.pdf$", file)
-		if matches == true {
-
-			jsonObj.Set(file)
-			fmt.Println(file)
-		}
-	}
-
-	//TODO return the list of files as a json structure
-
-	fmt.Println(jsonObj.String())
-}
-
-func requiredExtensionFiles(files []string) []string {
-
-	var reqFiles []string
-	for _, file := range files {
-
-		matches, _ := regexp.MatchString(".*.pdf$", file)
-		if matches == true {
-
-			fmt.Println(file)
-
-		}
-	}
-
 }
