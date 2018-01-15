dbdc278c6f569a5d4722a63b0583071032611ac3
Abhinav Sharma <abhi18av@gmail.com>
2018-01-15 22:37:39 +0530

    The functional requirement of status is complete

--- cmd/status.go
+++ cmd/status.go
@@ -29,7 +29,7 @@ var statusCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("status called")
 
-		fetchStatusDev()
+		fetchStatus()
 
 	},
 }
@@ -54,16 +54,12 @@ func init() {
 func fetchStatus() {
 
 	var ClientID = config.FetchAndValidate("ClientID") // returns string
-	fmt.Println(ClientID)
 
 	var ClientSecret = config.FetchAndValidate("ClientSecret") // returns string
-	fmt.Println(ClientSecret)
 
 	var ProjectKey = config.FetchAndValidate("ProjectKey") // returns string
-	fmt.Println(ProjectKey)
 
 	var AccessToken = config.FetchAndValidate("AccessToken") // returns string
-	fmt.Println(AccessToken)
 
 	resp, err := resty.R().
 		SetHeader("Accept", "application/json").
@@ -72,25 +68,6 @@ func fetchStatus() {
 			"client_id":     ClientID,
 			"client_secret": ClientSecret,
 			"project_key":   ProjectKey}).
-		Post("http://dev.devnagri.co.in/api/project/status")
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(resp)
-
-}
-
-func fetchStatusDev() {
-
-	resp, err := resty.R().
-		SetHeader("Accept", "application/json").
-		SetAuthToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE0MmI1YjlkOTI3ZGQ2ZjI3ODcyZDJmNDFmNWQzNjhkOGU3MmVhZjk2YzE2YTdmMTZjM2ZkM2I5ZmVmNjJiMDFmMTBiOWQwMTMxNzJjYTIyIn0.eyJhdWQiOiIxIiwianRpIjoiMTQyYjViOWQ5MjdkZDZmMjc4NzJkMmY0MWY1ZDM2OGQ4ZTcyZWFmOTZjMTZhN2YxNmMzZmQzYjlmZWY2MmIwMWYxMGI5ZDAxMzE3MmNhMjIiLCJpYXQiOjE1MTU1OTA4NDksIm5iZiI6MTUxNTU5MDg0OSwiZXhwIjoxNTQ3MTI2ODQ5LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.inJAzB_9Vjx9YXHQgZYCy3HxbTaSV2VSjKYgtB_s4ZrrS9vucEnEF4Ijh8wPMylxDLI7e8iGfRObK0rGTBqnCw94ElcmeYPM7GfWRqWW5Bz8ctJhXH7-VGml22fT6ahFVHssbVcx636xwPlydhN4fBay0TElXgC5QkSmzINFPMF3FRJ54WaAsTAvwpsnAIUfigZdNggSjMnROp-i7dtF5M9Ty3utLM2kmlEZVC9lhhLUvJujvbWJwCGDOStrBv4iapWQ4qPZ8AuaVTmigW_hVinkZ6GGTp8GT07GGF9iwDjPnSyCzEx7Ta4S0U3nSeJVogxmsO7N1hZrmCuZmUtEkSPMSP0wIhrMrranpzqFRqvsYve6hp8BBE9qxbuiEwc3F-2xFm-R6esmUu5mpWcRlM1imenuvb9C-lotKf0znM9neVimWhMe7OqENcGXTx7puTyZoK0wUmrn-2_vIxfwtU5s9aBomlw6qGQt6k14pgKA69bp2C51C46HyKa_QrEvol-_3YvccjvR3hO-jcAj_SLALYk6HJp1eSqy_tGZZ5QW4-4QvUcPaa3E4sXA4Ye_4OB2dGRcKVuhR5jzpaYJC-_qGRY0dhfYdpKjEgc1o3pitzpNW0iWzImN9iidRThu_YFP_RwPk2Qragch55f086JNdM7jGPkjcEUcgGIs3PE").
-		SetFormData(map[string]string{
-			"client_id":     "3",
-			"client_secret": "3WnUqVSP7Vhs8DU7FInIrwHIVMg9twGshcpswlJW",
-			"project_key":   "2e12635aca73c7d39ec76a514d7490a6"}).
 		Post("http://dev.devnagri.co.in/api/project/status")
 
 	if err != nil {
