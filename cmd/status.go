10ff2d061a9f95cd44016c01e5bb142e61732cfc
Abhinav Sharma <abhi18av@gmail.com>
2018-01-15 22:30:11 +0530

    working on the status command

--- cmd/status.go
+++ cmd/status.go
@@ -29,21 +29,17 @@ var statusCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("status called")
 
-		var ClientID = config.FetchAndValidate("ClientID") // returns string
+		//fetchStatus()
 
-		var ClientSecret = config.FetchAndValidate("ClientSecret") // returns string
-
-		var ProjectKey = config.FetchAndValidate("ProjectKey") // returns string
-
-		var AccessToken = config.FetchAndValidate("AccessToken") // returns string
+		fmt.Println("We are inside the status command")
 
 		resp, err := resty.R().
 			SetHeader("Accept", "application/json").
-			SetAuthToken(AccessToken).
+			SetAuthToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE0MmI1YjlkOTI3ZGQ2ZjI3ODcyZDJmNDFmNWQzNjhkOGU3MmVhZjk2YzE2YTdmMTZjM2ZkM2I5ZmVmNjJiMDFmMTBiOWQwMTMxNzJjYTIyIn0.eyJhdWQiOiIxIiwianRpIjoiMTQyYjViOWQ5MjdkZDZmMjc4NzJkMmY0MWY1ZDM2OGQ4ZTcyZWFmOTZjMTZhN2YxNmMzZmQzYjlmZWY2MmIwMWYxMGI5ZDAxMzE3MmNhMjIiLCJpYXQiOjE1MTU1OTA4NDksIm5iZiI6MTUxNTU5MDg0OSwiZXhwIjoxNTQ3MTI2ODQ5LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.inJAzB_9Vjx9YXHQgZYCy3HxbTaSV2VSjKYgtB_s4ZrrS9vucEnEF4Ijh8wPMylxDLI7e8iGfRObK0rGTBqnCw94ElcmeYPM7GfWRqWW5Bz8ctJhXH7-VGml22fT6ahFVHssbVcx636xwPlydhN4fBay0TElXgC5QkSmzINFPMF3FRJ54WaAsTAvwpsnAIUfigZdNggSjMnROp-i7dtF5M9Ty3utLM2kmlEZVC9lhhLUvJujvbWJwCGDOStrBv4iapWQ4qPZ8AuaVTmigW_hVinkZ6GGTp8GT07GGF9iwDjPnSyCzEx7Ta4S0U3nSeJVogxmsO7N1hZrmCuZmUtEkSPMSP0wIhrMrranpzqFRqvsYve6hp8BBE9qxbuiEwc3F-2xFm-R6esmUu5mpWcRlM1imenuvb9C-lotKf0znM9neVimWhMe7OqENcGXTx7puTyZoK0wUmrn-2_vIxfwtU5s9aBomlw6qGQt6k14pgKA69bp2C51C46HyKa_QrEvol-_3YvccjvR3hO-jcAj_SLALYk6HJp1eSqy_tGZZ5QW4-4QvUcPaa3E4sXA4Ye_4OB2dGRcKVuhR5jzpaYJC-_qGRY0dhfYdpKjEgc1o3pitzpNW0iWzImN9iidRThu_YFP_RwPk2Qragch55f086JNdM7jGPkjcEUcgGIs3PE").
 			SetFormData(map[string]string{
-				"client_id":     ClientID,
-				"client_secret": ClientSecret,
-				"project_key":   ProjectKey}).
+				"client_id":     "3",
+				"client_secret": "3WnUqVSP7Vhs8DU7FInIrwHIVMg9twGshcpswlJW",
+				"project_key":   "2e12635aca73c7d39ec76a514d7490a6"}).
 			Post("http://dev.devnagri.co.in/api/project/status")
 
 		if err != nil {
@@ -58,6 +54,9 @@ var statusCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(statusCmd)
 
+	//fmt.Println("Working on fetchStatusDev")
+	//fetchStatusDev()
+
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
@@ -67,4 +66,54 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func fetchStatus() {
+
+	var ClientID = config.FetchAndValidate("ClientID") // returns string
+	fmt.Println(ClientID)
+
+	var ClientSecret = config.FetchAndValidate("ClientSecret") // returns string
+	fmt.Println(ClientSecret)
+
+	var ProjectKey = config.FetchAndValidate("ProjectKey") // returns string
+	fmt.Println(ProjectKey)
+
+	var AccessToken = config.FetchAndValidate("AccessToken") // returns string
+	fmt.Println(AccessToken)
+
+	resp, err := resty.R().
+		SetHeader("Accept", "application/json").
+		SetAuthToken(AccessToken).
+		SetFormData(map[string]string{
+			"client_id":     ClientID,
+			"client_secret": ClientSecret,
+			"project_key":   ProjectKey}).
+		Post("http://dev.devnagri.co.in/api/project/status")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp)
+
+}
+
+func fetchStatusDev() {
+
+	resp, err := resty.R().
+		SetHeader("Accept", "application/json").
+		SetAuthToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE0MmI1YjlkOTI3ZGQ2ZjI3ODcyZDJmNDFmNWQzNjhkOGU3MmVhZjk2YzE2YTdmMTZjM2ZkM2I5ZmVmNjJiMDFmMTBiOWQwMTMxNzJjYTIyIn0.eyJhdWQiOiIxIiwianRpIjoiMTQyYjViOWQ5MjdkZDZmMjc4NzJkMmY0MWY1ZDM2OGQ4ZTcyZWFmOTZjMTZhN2YxNmMzZmQzYjlmZWY2MmIwMWYxMGI5ZDAxMzE3MmNhMjIiLCJpYXQiOjE1MTU1OTA4NDksIm5iZiI6MTUxNTU5MDg0OSwiZXhwIjoxNTQ3MTI2ODQ5LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.inJAzB_9Vjx9YXHQgZYCy3HxbTaSV2VSjKYgtB_s4ZrrS9vucEnEF4Ijh8wPMylxDLI7e8iGfRObK0rGTBqnCw94ElcmeYPM7GfWRqWW5Bz8ctJhXH7-VGml22fT6ahFVHssbVcx636xwPlydhN4fBay0TElXgC5QkSmzINFPMF3FRJ54WaAsTAvwpsnAIUfigZdNggSjMnROp-i7dtF5M9Ty3utLM2kmlEZVC9lhhLUvJujvbWJwCGDOStrBv4iapWQ4qPZ8AuaVTmigW_hVinkZ6GGTp8GT07GGF9iwDjPnSyCzEx7Ta4S0U3nSeJVogxmsO7N1hZrmCuZmUtEkSPMSP0wIhrMrranpzqFRqvsYve6hp8BBE9qxbuiEwc3F-2xFm-R6esmUu5mpWcRlM1imenuvb9C-lotKf0znM9neVimWhMe7OqENcGXTx7puTyZoK0wUmrn-2_vIxfwtU5s9aBomlw6qGQt6k14pgKA69bp2C51C46HyKa_QrEvol-_3YvccjvR3hO-jcAj_SLALYk6HJp1eSqy_tGZZ5QW4-4QvUcPaa3E4sXA4Ye_4OB2dGRcKVuhR5jzpaYJC-_qGRY0dhfYdpKjEgc1o3pitzpNW0iWzImN9iidRThu_YFP_RwPk2Qragch55f086JNdM7jGPkjcEUcgGIs3PE").
+		SetFormData(map[string]string{
+			"client_id":     "3",
+			"client_secret": "3WnUqVSP7Vhs8DU7FInIrwHIVMg9twGshcpswlJW",
+			"project_key":   "2e12635aca73c7d39ec76a514d7490a6"}).
+		Post("http://dev.devnagri.co.in/api/project/status")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp)
+
 }
