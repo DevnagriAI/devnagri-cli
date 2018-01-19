2126822d25342512488fc4f1014eab0d7be61134
Abhinav Sharma <abhi18av@gmail.com>
2018-01-19 13:11:41 +0530

    working on pretty printing the status

--- _dev/prettyPrint.go
+++ _dev/prettyPrint.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/Jeffail/gabs"
+	//	"reflect"
 )
 
 func main() {
 
-	jsonParsed, _ := gabs.ParseJSON([]byte(`{"languages_status":{"hi":{"translatedWordsCount":0,"reviewedWordsCount":0,"totalWordsCount":1283,"pendingWordsCount":1283}},"project_status":"draft"}`))
-	value := jsonParsed.Search("totalWordsCount").Data()
-	fmt.Println(value)
+	jsonString := []byte(`
+{"languages_status":
+{"hi":
+{"translatedWordsCount":0,"reviewedWordsCount":0,"totalWordsCount":1283,"pendingWordsCount":1283}},
+"project_status":"draft"}
+`)
 
+	jsonParsed, _ := gabs.ParseJSON(jsonString)
+
+	fmt.Println(jsonParsed.Path("languages_status").String())
+
+	//projectStatus := jsonParsed.Path("project_status").Data().(string)
+
+	//value := jsonParsed.Path("languages_status.hi.totalWordsCount").Data()
+
+	//fmt.Println(value)
+
+	//fmt.Println(reflect.TypeOf(value))
+
+	/*
+			lang, _ := jsonParsed.Path("languages_status").Children()
+			langName := lang[0]
+			fmt.Println(langName)
+
+		children, _ := jsonParsed.Path("languages_status").Children()
+		for _, child := range children {
+			fmt.Println(child.Data())
+		}
+
+	*/
 }
